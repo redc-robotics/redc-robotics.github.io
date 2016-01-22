0047cc96a99b58f7f41263d425cc98f0bef2786e
jc3m <jonathanj.xu@yahoo.com>
2016-01-22 12:03:20 -0600

    Created users controller

--- rails-hydro/db/schema.rb
+++ rails-hydro/db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122173613) do
+ActiveRecord::Schema.define(version: 20160122175625) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "last_name"
   end
 
 end
